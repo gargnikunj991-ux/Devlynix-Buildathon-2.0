3755ed25d681385cf73cf2116548e16e685a6771
Nikunj garg <gargnikunj991@gmail.com>
2026-06-15 16:45:28 +0530

    Fix websocket build

--- src/main/java/com/devtinder/dto/request/ChatMessageRequest.java
+++ src/main/java/com/devtinder/dto/request/ChatMessageRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record ChatMessageRequest(
+        long matchId,
         String senderEmail,
         @NotBlank @Size(max = 1200) String content
 ) {
